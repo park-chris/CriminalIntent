15d874d7a61cea4097923d3f37e072ce3d2643d1
박수정 <zxzx4342@naver.com>
2022-02-24 21:53:47 +0900

    crime 데이터 모델 클래스 생성

---
+++ app/src/main/java/com/crystal/android/criminalintent/Crime.kt
@@ -0,0 +1,10 @@
+package com.crystal.android.criminalintent
+
+import java.util.*
+
+// UUID는 안드로이드 프레임워크에 포함된 유틸리티 클래스로 고유한 ID 값을 쉽게 생성하는 방법 제공
+data class Crime(val id: UUID = UUID.randomUUID(),
+                 var title: String ="",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false) {
+}
